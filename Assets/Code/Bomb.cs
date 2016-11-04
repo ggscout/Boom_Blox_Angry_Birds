c8904b0a730cd7b6c0ade2288beecad6a8d9bfaa
tieyongyu <tieyongyu@Tieyongs-MacBook-Pro.local>
2016-11-04 02:54:28 -0500

    Boom_Blox
    Simple implementation of game mechanics of video game Boom Blox
    developed by EA in 2008.

---
+++ Assets/Code/Bomb.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    public float ThresholdForce = 2;
+    public GameObject ExplosionPrefab;
+
+	/// <summary>
+	/// generate the repulsive force of explosion; Hide the Explosive box and instantiate the explosion
+	/// prefab; also destruct the explosive box after 0.1s
+	/// </summary>
+	private void Boom(){
+		this.gameObject.GetComponent<PointEffector2D> ().enabled = true;
+		this.gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+		Invoke ("Destruct", 0.1f);
+	}
+
+	/// <summary>
+	/// if the relative velocity exceeds thresholdForce, call Boom()
+	/// </summary>
+	/// <param name="obj">Object the explosive box collides with</param>
+	private void OnCollisionEnter2D(Collision2D obj) {
+		if (obj.relativeVelocity.magnitude > ThresholdForce) {
+			Boom ();
+		}
+	}
+	/// <summary>
+	/// Destroy the Explosive Box.
+	/// </summary>
+	private void Destruct() {
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Code/TargetBox.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class TargetBox : MonoBehaviour
+{
+    /// <summary>
+    /// Targets that move past this point score automatically.
+    /// </summary>
+    public static float OffScreen;
+	/// <summary>
+	/// check whether the score has been awarded to the player
+	/// </summary>
+	public bool ScoreAdded = false;
+
+    internal void Start() {
+        OffScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-100, 0, 0)).x;
+    }
+
+    internal void Update()
+    {
+        if (transform.position.x > OffScreen)
+            Scored();
+    }
+
+	/// <summary>
+	/// change the color of the target box; add score;
+	/// </summary>
+    private void Scored()
+    {
+		this.gameObject.GetComponent<SpriteRenderer> ().color = Color.green;
+		if (!ScoreAdded) {
+			ScoreKeeper.AddToScore (this.gameObject.GetComponent<Rigidbody2D>().mass);
+			ScoreAdded = true;
+		}
+    }
+
+	/// <summary>
+	/// call Scored() if the target box hits ground
+	/// </summary>
+	/// <param name="obj">Object TargetBox collides with</param>
+	private void OnCollisionEnter2D(Collision2D obj) {
+		if (obj.gameObject.tag.Equals("Ground")) {
+			Scored ();
+		}
+	}
+}
